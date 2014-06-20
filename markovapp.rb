a535c9de1f38ca5eebda08941eacde3906b167d1
Rachel Prince <rachel.prince@yale.edu>
2014-06-20 13:54:24 -0400

    took away user input

--- markovapp.rb
+++ markovapp.rb
@@ -16,13 +16,13 @@ end
 post '/' do
 	input = params[:input]
 
-	new_proverb = ''
+	new_proverb = generate
 
-	if input == ''
-		new_proverb=generate
-	else
-		new_proverb=generate_with_input(input)
-	end
+	#if input == '' || input=='optional text'
+	#	new_proverb=generate
+	#else
+#		new_proverb=generate_with_input(input)
+#	end
 
 	session[:proverbs].push(new_proverb)
 	if session[:proverbs].length > 10
